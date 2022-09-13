be644e22a5531acf16467ea84114bfe3060d4fda
jmagistrado <joycemagistrado@gmail.com>
2022-09-13 16:22:12 -0700

    Robot Project Complete

---
+++ src/components/BadRobot.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BadRobot = (props) => {
+    return (
+      <>
+      <h2>Bad Robot:</h2>
+      <p>I'm sorry, but all I hear you saying is {props.badInput}. Is this correct?</p>
+      </>
+    )
+  }
+  
+  
+  
+  
+  export default BadRobot;
